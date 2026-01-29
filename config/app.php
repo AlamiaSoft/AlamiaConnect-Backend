5a0940e73d82e2c4ab8c4c678013022c59866892
amrshah <amr.shah@gmail.com>
2026-01-30 00:31:17 +0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -83,7 +83,7 @@ return [
     |
      */
 
-    'timezone' => env('APP_TIMEZONE', 'Asia/Kolkata'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Karachi'),
 
     /*
     |--------------------------------------------------------------------------
