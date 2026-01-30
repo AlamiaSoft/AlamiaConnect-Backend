4d8b1bdb5bc5903ae96d271c34b981e62babe0e4
amrshah <amr.shah@gmail.com>
2026-01-30 11:09:44 +0500

    correction to profile pic url

--- packages/Webkul/Admin/src/Resources/views/components/layouts/header/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/header/index.blade.php
@@ -79,11 +79,16 @@
             <!-- Admin Dropdown -->
             <x-slot:content class="mt-2 border-t-0 !p-0">
                 <div class="flex items-center gap-1.5 border border-x-0 border-b-gray-300 px-5 py-2.5 dark:border-gray-800">
-                    <img
-                        src="{{ url('cache/logo.png') }}"
-                        width="24"
-                        height="24"
-                    />
+                    @if ($user->image)
+                        <img
+                            src="{{ $user->image_url }}"
+                            class="h-6 w-6 rounded-full object-cover"
+                        />
+                    @else
+                        <div class="flex h-6 w-6 items-center justify-center rounded-full bg-pink-400 text-[10px] font-semibold leading-6 text-white text-center uppercase">
+                            {{ substr($user->name, 0, 1) }}
+                        </div>
+                    @endif
 
                     <!-- Version -->
                     <p class="text-gray-400">
