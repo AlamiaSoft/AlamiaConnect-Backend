ea9046c8826892e928b873e1d00a36fe1de1c4dc
amrshah <amr.shah@gmail.com>
2026-02-11 15:24:24 +0500

    Fix: Grant users/roles and automation permissions to restricted roles in all seeders

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -43,11 +43,22 @@ class RoleSeeder extends Seeder
                     "contacts.persons.edit", "contacts.persons.delete", "contacts.persons.view",
                     "contacts.organizations", "contacts.organizations.create", "contacts.organizations.edit",
                     "contacts.organizations.delete", "products", "products.create", "products.edit",
-                    "products.delete", "products.view", "settings", "settings.lead", "settings.lead.pipelines",
+                    "products.delete", "products.view", "settings", 
+                    "settings.user", "settings.user.groups", "settings.user.groups.create", "settings.user.groups.edit", "settings.user.groups.delete",
+                    "settings.user.roles", "settings.user.roles.create", "settings.user.roles.edit", "settings.user.roles.delete",
+                    "settings.user.users", "settings.user.users.create", "settings.user.users.edit", "settings.user.users.delete",
+                    "settings.lead", "settings.lead.pipelines",
                     "settings.lead.pipelines.create", "settings.lead.pipelines.edit", "settings.lead.pipelines.delete",
                     "settings.lead.sources", "settings.lead.sources.create", "settings.lead.sources.edit",
                     "settings.lead.sources.delete", "settings.lead.types", "settings.lead.types.create",
-                    "settings.lead.types.edit", "settings.lead.types.delete", "settings.other_settings",
+                    "settings.lead.types.edit", "settings.lead.types.delete", 
+                    "settings.automation", "settings.automation.attributes", "settings.automation.attributes.create", "settings.automation.attributes.edit", "settings.automation.attributes.delete",
+                    "settings.automation.email_templates", "settings.automation.email_templates.create", "settings.automation.email_templates.edit", "settings.automation.email_templates.delete",
+                    "settings.automation.workflows", "settings.automation.workflows.create", "settings.automation.workflows.edit", "settings.automation.workflows.delete",
+                    "settings.automation.events", "settings.automation.events.create", "settings.automation.events.edit", "settings.automation.events.delete",
+                    "settings.automation.campaigns", "settings.automation.campaigns.create", "settings.automation.campaigns.edit", "settings.automation.campaigns.delete",
+                    "settings.automation.webhooks", "settings.automation.webhooks.create", "settings.automation.webhooks.edit", "settings.automation.webhooks.delete",
+                    "settings.other_settings",
                     "settings.other_settings.web_forms", "settings.other_settings.web_forms.view",
                     "settings.other_settings.web_forms.create", "settings.other_settings.web_forms.edit",
                     "settings.other_settings.web_forms.delete", "settings.other_settings.tags",
@@ -56,6 +67,7 @@ class RoleSeeder extends Seeder
                     "settings.data_transfer.imports.create", "settings.data_transfer.imports.edit",
                     "settings.data_transfer.imports.delete", "settings.data_transfer.imports.import"
                 ],
+
             ]
         );
 
